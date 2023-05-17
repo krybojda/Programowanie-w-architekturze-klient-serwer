316fc8c55ee63c681046a3b40034b7a631198cf8
krybojda <kbojda2000@gmail.com>
2023-05-17 13:29:05 +0200

    Update zamowienia.php

--- zamowienia.php
+++ zamowienia.php
@@ -5,17 +5,17 @@
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css" integrity="sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO" crossorigin="anonymous">
     <link rel="stylesheet" href="css.css">
     <script src="cdnjs.cloudflare.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
-    <title>Zamówienia</title>
+    <title>Rezerwacje</title>
   </head>
   <body>
     <div class="sidenav">
-      <a href="index.php"><img src="logo1.png"></a>
-      <a href="index.php?typ=kardiologia">Kardiologia</a>
-      <a href="index.php?typ=neurologia">Neurologia</a>
-      <a href="index.php?typ=okulistyka">Okulistyka</a>
-      <a href="index.php?typ=pediatria">Pediatria</a>
-      <a href="index.php?typ=alergologia">Alergologia</a>
-      <a href="zamowienia.php">Zamówienia</a>
+      <a href="index.php"><img src="logo.png"></a>
+      <a href="index.php?typ=Kardiologia">Kardiologia</a>
+      <a href="index.php?typ=Neurologia">Neurologia</a>
+      <a href="index.php?typ=Okulistyka">Okulistyka</a>
+      <a href="index.php?typ=Pediatria">Pediatria</a>
+      <a href="index.php?typ=Alergologia">Alergologia</a>
+      <a href="Rezerwacje.php">Rezerwacje</a>
       <a href="koszyk.php">Koszyk</a>
       <?php
       session_start();
@@ -47,19 +47,19 @@
         <?php
           $con = mysqli_connect("localhost","root","","naszlekarz");
           $idusera = $_SESSION["iduser"];
-          $sql = "SELECT * FROM zamowienia WHERE iduser = $idusera";
+          $sql = "SELECT * FROM Rezerwacje WHERE iduser = $idusera";
           $query = mysqli_query($con,$sql);
           if (mysqli_num_rows($query) > 0) {
             echo '<table class="jq-toggle-table table table-condensed table-striped">';
-            echo "<tr><th>ID Zamówienia</th><th>Typ</th><th>Nazwa</th><th>Cena</th><th>Suma</th><th>Zwróć</th></tr>";
+            echo "<tr><th>ID Rezerwacje</th><th>Typ</th><th>Nazwa</th><th>Cena</th><th>Suma</th><th>Zwróć</th></tr>";
               while($row = mysqli_fetch_assoc($query)) {
                       echo "<tr>";
-                      echo "<td class='text-center'>" .  $row["id_zamowienia"] . "</td>";
+                      echo "<td class='text-center'>" .  $row["id_Rezerwacje"] . "</td>";
                       echo "<td class='text-center'>" .  $row["tabela"] . "</td>";
                       echo "<td class='text-center'>" .  $row["nazwa"] . "</td>";
                       echo "<td class='text-center'>" .  $row["cena"] . "</td>";
                       echo "<td class='text-center'>" .  $row["suma"] . "</td>";
-                      echo "<td class='text-center'><button onclick='url(" . $row["id_zamowienia"] . ")'>Zwróć</button></td>";
+                      echo "<td class='text-center'><button onclick='url(" . $row["id_Rezerwacje"] . ")'>Zwróć</button></td>";
                       echo "</td>";
               }
               echo "</table>";
